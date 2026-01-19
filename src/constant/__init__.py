8058d71852c699a63eb2aa49d2c8e85007c41e08
deb179 <debi123patra@gmail.com>
2026-01-19 12:15:50 +0530

    updated

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "water-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="MONGO"
+
+MODEL_FILE_NAME ="model"
+MODEL_FILE_EXTENTION =".pkl"
